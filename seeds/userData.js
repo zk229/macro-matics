62f908caba53a15baa731a180852cbeb52c815fe
zk229 <zkirsche01@gmail.com>
2022-03-29 20:54:11 -0400

    add seeds

---
+++ seeds/userData.js
@@ -0,0 +1,16 @@
+const { User } = require("../models");
+
+const userData = [
+    {
+        username: "test",
+        email: "test@test.com",
+        password: "abc123",
+        name: "Test Testerson",
+        height: 72.5,
+        weight: 200
+    }
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
