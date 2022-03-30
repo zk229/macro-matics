664d467337c3464a022b702ca4b56817e6b2e47b
zk229 <zkirsche01@gmail.com>
2022-03-29 20:41:18 -0400

    add Workout model and relations

---
+++ models/Workout.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Workout extends Model {}
+
+Workout.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        calories: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "workout"
+    }
+);
+
+module.exports = Workout;
