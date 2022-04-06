821c43f189cc37f3f619387ae0f2bbec2709c160
jrwj97 <jrwj97@aol.com>
2022-04-06 00:59:22 -0400

    added styling to most pages

--- models/Meal.js
+++ models/Meal.js
@@ -4,50 +4,6 @@ const sequelize = require("../config/connection");
 class Meal extends Model {}
 
 Meal.init(
-<<<<<<< HEAD
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    calories: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    protein: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    carbs: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    fat: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "meal",
-  }
-=======
     {
         id: {
             type: DataTypes.INTEGER,
@@ -90,7 +46,6 @@ Meal.init(
         underscored: true,
         modelName: "meal"
     }
->>>>>>> cf3e765dbd6f2f845860b9c154c10cdf31c1a9fd
 );
 
 module.exports = Meal;
