20c95bd80636f76c2699c97b4a4e857a90c40b53
zk229 <zkirsche01@gmail.com>
2022-04-07 18:17:38 -0400

    add route for viewing other dates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,6 +32,11 @@ router.get("/add-workout", auth, (req, res) => {
 
 router.get("/view-stats", auth, async (req, res) => {
     const date = DateTime.now().toISODate();
+    res.redirect("/view-stats/" + date)
+});
+
+router.get("/view-stats/:date", auth, async (req, res) => {
+    const date = req.params.date;
     let meals = await Meal.findAll({
         where: {
             date: date,
